98f615a2be40cea9dd50bc9c4a7df08a2336e2c0
daneecool <daneereiji@gmail.com>
2025-03-17 17:27:43 +0900

    correction for "Remove navsidebar"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,11 +70,11 @@ const config = {
             label: 'Tutorial',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
         ],
       },
       footer: {
