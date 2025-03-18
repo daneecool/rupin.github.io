8447870e8baaadc95bf656c10ecbb0635dba56b3
daneecool <daneereiji@gmail.com>
2025-03-18 15:49:16 +0900

    LocalTest Passed for GTAG

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -133,6 +133,24 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      headTags: [
+        {
+          tagName: 'script',
+          attributes: {
+            async: true,
+            src: 'https://www.googletagmanager.com/gtag/js?id=G-X4C3WEWV1Z',
+          },
+        },
+        {
+          tagName: 'script',
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-X4C3WEWV1Z');
+          `,
+        },
+      ],
     }),
 };
 
