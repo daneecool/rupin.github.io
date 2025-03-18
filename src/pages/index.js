3a0e8ae98a59c1b5bd9f9c3f569a3699a3d169ed
daneecool <daneereiji@gmail.com>
2025-03-18 15:25:32 +0900

    Re-test Google Analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import Analytics from '@site/src/components/Analytics'; // Import the Analytics component
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -28,34 +28,11 @@ function HomepageHeader() {
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
-
-  useEffect(() => {
-    // Add the Google Analytics script dynamically
-    const script1 = document.createElement('script');
-    script1.async = true;
-    script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-X4C3WEWV1Z';
-    document.head.appendChild(script1);
-
-    const script2 = document.createElement('script');
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-X4C3WEWV1Z');
-    `;
-    document.head.appendChild(script2);
-
-    return () => {
-      // Cleanup scripts if needed
-      document.head.removeChild(script1);
-      document.head.removeChild(script2);
-    };
-  }, []);
-
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
+      <Analytics /> {/* Add the Analytics component */}
       <HomepageHeader />
       <main>
         <HomepageFeatures />
