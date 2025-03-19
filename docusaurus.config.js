660f3b8887c7eee8da2543513541962c27950c22
danee <daneereiji@gmail.com>
2025-03-20 02:07:28 +0900

    error fix 00

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '<br><br> ルパンへようこそ  ',
+  title: 'ルパンへようこそ',
   // tagline: 'Welcome message to the shop',
   url: 'https://your-docusaurus-test-site.com', // website url = dns
   baseUrl: '/test.github.io/',
